d80be81f8a677f2dc613be5ff850b9e2cb9deba9
Iana <iana.li.timofeevna@gmail.com>
2025-01-10 11:59:42 +0100

    [add fetch data]

--- src/services/api.js
+++ src/services/api.js
@@ -1,43 +1,61 @@
 import { USER_MAIN_DATA, USER_ACTIVITY, USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from '../../sportsee-backend/app/data';
 import UserDataModel from "./models/UserDataModel";
 
+const BASE_URL = 'http://localhost:5173'
+
 export const fetchUserMainData = async (userId) => {
     try {
-        const userData = USER_MAIN_DATA.find(user => user.id === Number(userId));
-        if (!userData) throw new Error('User not found');
-        return new UserDataModel(userData);
+        const response = await fetch(`${BASE_URL}/user/${userId}`);
+        if (!response.ok) throw new Error('User not found');
+        const data = await response.json();
+        return new UserDataModel(data);
     } catch (error) {
-        throw new Error(`Error fetching user data: ${error.message}`);
+        console.warn(`Error fetching user data: ${error.message}`);
+        const mockData = USER_MAIN_DATA.find((user) => user.id === Number(userId));
+        if (!mockData) throw new Error('Mock user data not found');
+        return new UserDataModel(mockData);
     }
 };
 
 export const fetchUserActivity = async (userId) => {
     try {
-        const activity = USER_ACTIVITY.find(item => item.userId === Number(userId));
-        if (!activity) throw new Error('Activity not found');
-        return UserDataModel.formatActivity(activity);
+        const response = await fetch(`${BASE_URL}/user/${userId}/activity`);
+        if (!response.ok) throw new Error('Activity not found');
+        const data = await response.json()
+        return UserDataModel.formatActivity(data);
     } catch (error) {
-        throw new Error(`Error fetching activity data: ${error.message}`);
+        console.warn(`Error fetching activity data: ${error.message}`);
+        const mockData = USER_ACTIVITY.find((user) => user.userId === Number(userId));
+        if (!mockData) throw new Error('Mock user data not found');
+        return UserDataModel.formatActivity(mockData);
     }
 };
 
 export const fetchUserAverageSessions = async (userId) => {
     try {
-        const sessions = USER_AVERAGE_SESSIONS.find(item => item.userId === Number(userId));
-        if (!sessions) throw new Error('Sessions not found');
-        return UserDataModel.formatAverageSessions(sessions);
+        const response = await fetch(`${BASE_URL}/user/${userId}/sessions`);
+        if (!response.ok) throw new Error('Sessions not found');
+        const data = await response.json();
+        return UserDataModel.formatAverageSessions(data);
     } catch (error) {
-        throw new Error(`Error fetching sessions data: ${error.message}`);
+         console.warn(`Error fetching sessions data: ${error.message}`);
+        const mockData = USER_AVERAGE_SESSIONS.find((user) => user.userId === Number(userId));
+        if (!mockData) throw new Error('Mock user data not found');
+        return UserDataModel.formatAverageSessions(mockData);
     }
 };
 
 export const fetchUserPerformance = async (userId) => {
     try {
-        const performance = USER_PERFORMANCE.find(item => item.userId === Number(userId));
-        if (!performance) throw new Error('Performance not found');
-        return UserDataModel.formatPerformance(performance);
+        const response = await fetch(`${BASE_URL}/user/${userId}/performance`);
+        if (!response.ok) throw new Error("Performance not found");
+        const data = await response.json();
+        return UserDataModel.formatPerformance(data);
     } catch (error) {
-        throw new Error(`Error fetching performance data: ${error.message}`);
+        console.warn(`Error fetching performance data from server: ${error.message}`);
+        const mockData = USER_PERFORMANCE.find((user) => user.userId === Number(userId));
+        if (!mockData) throw new Error("Mock performance data not found");
+        return UserDataModel.formatPerformance(mockData);
     }
 };
 
@@ -47,16 +65,16 @@ export const getAllUserData = async (userId) => {
             fetchUserMainData(userId),
             fetchUserActivity(userId),
             fetchUserAverageSessions(userId),
-            fetchUserPerformance(userId)
+            fetchUserPerformance(userId),
         ]);
 
         return {
             mainData,
             activity,
             sessions,
-            performance
+            performance,
         };
     } catch (error) {
-        throw error;
+        throw new Error(`Error fetching all user data: ${error.message}`);
     }
 };
