bfea436ca0c648031aee0923b8089a6f4cd26628
Iana <iana.li.timofeevna@gmail.com>
2025-01-14 12:55:43 +0100

    [try to fix deployment]

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/sportSee/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
