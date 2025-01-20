e935a5e7a91c9ceb7b661d8f119079c89882454d
Iana <iana.li.timofeevna@gmail.com>
2025-01-20 11:23:58 +0100

    [change sass in scorebar

--- src/components/score/score_bar/ScoreBar.jsx
+++ src/components/score/score_bar/ScoreBar.jsx
@@ -4,11 +4,12 @@ import calories from '../../../assets/icons_scorebar/calories.png'
 import carbs from '../../../assets/icons_scorebar/carbs.png'
 import fat from '../../../assets/icons_scorebar/fat.png'
 import protein from '../../../assets/icons_scorebar/protein.png'
+import './ScoreBar.css'
 
 export default function ScoreBar({ score }) {
 
   return (
-    <div>
+    <div className='scorebar-container'>
         <NutritionScore item={score.calories.value} icon={calories} name="Calories" measure={score.calories.unit} />
         <NutritionScore item={score.protein.value} icon={protein} name="Proteines" measure={score.protein.unit} />
         <NutritionScore item={score.carbs.value} icon={carbs} name="Clucides" measure={score.carbs.unit} />
